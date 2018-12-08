a1e6f86134c49a29ca6a9d52bca9e627270282df
Yingnan Ju <bunny_gg@hotmail.com>
2018-12-08 21:42:06 +0800

    tried slice method

---
+++ utils/mat_slice_generator.py
@@ -0,0 +1,91 @@
+import numpy as np
+import os
+import librosa
+from scipy import io as sio
+
+'''
+modality_index = 0:         01 = full-AV, 02 = video-only, 03 = audio-only
+vocal_channel_index = 1:    01 = speech, 02 = song
+emotion_index = 2:          01 = neutral, 02 = calm, 03 = happy, 04 = sad, 
+                            05 = angry, 06 = fearful, 07 = disgust, 08 = surprised
+intensity_index = 3:        01 = normal, 02 = strong
+statement_index = 4:        01 = "Kids are talking by the door", 02 = "Dogs are sitting by the door"
+repetition_index = 5:       01 = 1st repetition, 02 = 2nd repetition
+actor_index = 6:            01 to 24. Odd numbered actors are male, even numbered actors are female
+'''
+
+
+def read_wav_files(path, slice_length=0.5, step=0.25):
+    cate = [path + folder for folder in os.listdir(path) if os.path.isdir(path + folder)]
+    audio_list = []
+    sr_list = []
+    slice_id_list = []
+    file_id_list = []
+    meta_info_lists = [[], [], [], [], [], [], []]
+    separator = '-'
+    clip_threshold = 0.1
+
+    count = 0
+    for idx, folder in enumerate(cate):
+        print('reading the audios:%s' % folder)
+        for file_name in os.listdir(folder):
+            if not os.path.isfile(os.path.join(folder, file_name)):
+                continue
+            file_path = os.path.join(folder, file_name)
+            audio, sr_audio = librosa.load(file_path, sr=None)
+            start = 0
+            end = int(start + slice_length * sr_audio)
+            slice_id = 0
+            while end < len(audio):
+                audio_clip = audio[start:end]
+                # print(np.linalg.norm(audio_clip))
+                if np.linalg.norm(audio_clip) > clip_threshold:
+                    audio_list.append(audio_clip)
+                    sr_list.append(sr_audio)
+                    for i in range(len(meta_info_lists)):
+                        file_name_prefix = file_name.split('.')[0]
+                        meta_info = str(file_name_prefix).split(separator)[i]
+                        meta_info_lists[i].append(meta_info)
+                    slice_id_list.append(slice_id)
+                    file_id_list.append(count)
+                slice_id += 1
+                start += int(step * sr_audio)
+                end = start + int(slice_length * sr_audio)
+            count += 1
+            if count % 10 == 0:
+                print("\rreading {0}/{1}".format(count, len(os.listdir(folder))), end='')
+        print('\r', end='')
+    for meta_info_list in meta_info_lists:
+        assert len(meta_info_list) == len(audio_list)
+
+    audio_array = np.asarray(audio_list, np.float32)
+    return audio_array, np.asarray(sr_list, int), np.asarray(file_id_list, int), np.asarray(slice_id_list, int), \
+           np.asarray(meta_info_lists, int)
+
+
+def normalize_features(data, v_max=1.0, v_min=0.0):
+    data_array = np.asarray(data, np.float32)
+    mins = np.min(data_array, axis=0)
+    maxs = np.max(data_array, axis=0)
+    rng = maxs - mins
+    result = v_max - ((v_max - v_min) * (maxs - data_array) / rng)
+    return result
+
+
+if __name__ == '__main__':
+    raw_file_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+    np.seterr(all='ignore')
+    raw_mat, sample_rates, file_ids, slice_ids, meta_info_labels = read_wav_files(raw_file_path)
+
+    sio.savemat(raw_file_path + 'raw_slice_050_025.mat', mdict={'feature_matrix': raw_mat,
+                                                                'sample_rate': sample_rates,
+                                                                'file_id': file_ids,
+                                                                'slice_id': slice_ids,
+                                                                'modality_label': meta_info_labels[0],
+                                                                'vocal_channel_label': meta_info_labels[1],
+                                                                'emotion_label': meta_info_labels[2],
+                                                                'intensity_label': meta_info_labels[3],
+                                                                'statement_label': meta_info_labels[4],
+                                                                'repetition_label': meta_info_labels[5],
+                                                                'actor_label': meta_info_labels[6],
+                                                                })
---
+++ utils/stft_slice.py
@@ -0,0 +1,75 @@
+import numpy as np
+import math
+from scipy import signal
+# import matplotlib.pyplot as plt
+from scipy import io
+
+
+def calculate_dft_matrix(n):
+    exp1, exp2 = np.meshgrid(np.arange(n), np.arange(n))
+    omega = np.exp(- 2 * math.pi * 1J / n)
+    w = np.power(omega, exp1 * exp2)
+    return w
+
+
+def get_framed_matrix(a, window_type, frame_size):
+    result = []
+    window = signal.get_window(window_type, frame_size)
+    start = 0
+    while start + frame_size <= len(a):
+        result.append(a[start:start + frame_size] * window)
+        start += frame_size // 2
+    result = np.asarray(result, np.float32).transpose()
+    return result
+
+
+def stft(a, window_type, frame_size):
+    a_frames = get_framed_matrix(a, window_type, frame_size)
+    a_y_mat = dft_mat.dot(a_frames)
+    return a_y_mat
+
+
+root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+mat_path = root_path + 'raw_slice_050_025.mat'
+digits = io.loadmat(mat_path)
+X, y, z, sr, file_ids, slice_ids, rep = digits.get('feature_matrix'), digits.get('emotion_label')[0], \
+                                        digits.get('intensity_label')[0], digits.get('sample_rate')[0], \
+                                        digits.get('file_id')[0], digits.get('slice_id')[0], \
+                                        digits.get('repetition_label')[0]
+n_samples, n_features = X.shape
+
+N = 256
+win_type = 'hann'
+
+dft_mat = calculate_dft_matrix(N)
+
+# fig = plt.figure()
+#
+# ax = fig.add_subplot(121)
+# spectrogram = np.absolute(stft(X[1], win_type, N))
+# ax.imshow(spectrogram[:len(spectrogram) // 2 + 1], vmax=0.1, vmin=-0.1)
+# ax.set_aspect(0.2)
+# ax.set_title('spectrogram')
+#
+# plt.show()
+
+stft_list = []
+
+count = 0
+for i in range(len(X)):
+    _data = X[i]
+    _sr = sr[i]
+    spectrogram = np.absolute(stft(X[i], win_type, N))
+    stft_list.append(spectrogram[:len(spectrogram) // 2 + 1])
+    count += 1
+    print("\rreading {0}/{1}".format(count, len(X)), end='')
+
+# 129 x 186
+io.savemat(root_path + 'stft_slice_256.mat', mdict={'feature_matrix': np.array(stft_list),
+                                                    'sample_rate': sr,
+                                                    'file_id': file_ids,
+                                                    'slice_id': slice_ids,
+                                                    'emotion_label': y,
+                                                    'intensity_label': z,
+                                                    'repetition_label': rep
+                                                    })
