17d8774a0a3bc5692d9cdb04b3f1e75b3a72650c
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-21 14:17:32 -0400

    extract model parameter to another file

---
+++ classification/model_parameter.py
@@ -0,0 +1,8 @@
+def get_parameter_149_26():
+    h = 149
+    w = 26
+    kernel_size = (5, 5)
+    kernel_stride = (1, 1)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
