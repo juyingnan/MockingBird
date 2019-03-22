ef15fd7cd6715bf25c8c2c244a48a95eac9d6eba
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-22 14:10:54 -0400

    added stft parameters

--- classification/model_parameter.py
+++ classification/model_parameter.py
@@ -16,6 +16,33 @@ class Logger(object):
         # you might want to specify some extra behavior here.
         pass
 
+
+def select_parameter(_h, _w):
+    # mfcc and logf
+    if _w == 26:
+        if _h == 526:
+            return get_parameter_526_26()
+        if _h == 149:
+            return get_parameter_149_26()
+        if _h == 99:
+            return get_parameter_99_26()
+        if _h == 49:
+            return get_parameter_49_26()
+
+    # stft
+    if _h == 257:
+        if _w == 95:
+            return get_parameter_257_95()
+        if _w == 189:
+            return get_parameter_257_189()
+        if _w == 283:
+            return get_parameter_257_283()
+        if _w == 990:
+            return get_parameter_257_990()
+
+    return None
+
+
 def get_parameter_526_26():
     h = 526
     w = 26
@@ -53,4 +80,44 @@ def get_parameter_49_26():
     kernel_stride = (1, 1)
     pool_stride = (2, 1)
     pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_257_95():
+    h = 257
+    w = 95
+    kernel_size = (5, 5)
+    kernel_stride = (2, 2)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 2), (2, 2), (2, 2), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_257_189():
+    h = 257
+    w = 189
+    kernel_size = (5, 5)
+    kernel_stride = (2, 2)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 2), (2, 2), (2, 2), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_257_283():
+    h = 257
+    w = 283
+    kernel_size = (5, 5)
+    kernel_stride = (2, 2)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 2), (2, 2), (2, 2), (2, 4)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_257_990():
+    h = 257
+    w = 990
+    kernel_size = (5, 5)
+    kernel_stride = (2, 2)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 2), (2, 4), (2, 4), (2, 4)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
