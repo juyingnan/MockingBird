16189588c5091eae88c8a51b3299911a33a98dd8
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-21 23:54:30 -0400

    updated parameter for 49,99,526

--- classification/model_parameter.py
+++ classification/model_parameter.py
@@ -16,11 +16,40 @@ class Logger(object):
         # you might want to specify some extra behavior here.
         pass
 
+def get_parameter_526_26():
+    h = 526
+    w = 26
+    kernel_size = (5, 5)
+    kernel_stride = (1, 1)
+    pool_stride = (4, 1)
+    pool_size_list = [(4, 1), (4, 1), (2, 1), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
 
 def get_parameter_149_26():
     h = 149
     w = 26
     kernel_size = (5, 5)
+    kernel_stride = (1, 1)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_99_26():
+    h = 99
+    w = 26
+    kernel_size = (5, 5)
+    kernel_stride = (1, 1)
+    pool_stride = (2, 1)
+    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
+
+
+def get_parameter_49_26():
+    h = 49
+    w = 26
+    kernel_size = (3, 3)
     kernel_stride = (1, 1)
     pool_stride = (2, 1)
     pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
