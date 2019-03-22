13a8e37297521e953e36a1f49816bab552ebf2cb
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-21 23:32:26 -0400

    added system args input override

--- classification/keras_classification_train.py
+++ classification/keras_classification_train.py
@@ -15,55 +15,56 @@ import model_parameter
 import logging
 import sys
 
+
+def get_cnn_model():
+    model = Sequential()
+    # Layer 1
+    model.add(Conv2D(32,
+                     kernel_size=kernel_size,
+                     strides=kernel_stride,
+                     activation='relu',
+                     input_shape=input_shape))
+    # model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(MaxPooling2D(pool_size=pool_size_list[0], strides=pool_stride))
+    # Layer 2
+    model.add(Conv2D(64, kernel_size, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(MaxPooling2D(pool_size=pool_size_list[1]))
+    # Layer 3
+    model.add(Conv2D(128, kernel_size, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(MaxPooling2D(pool_size=pool_size_list[2]))
+    # Layer 4
+    model.add(Conv2D(256, kernel_size, activation='relu'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(MaxPooling2D(pool_size=pool_size_list[3]))
+    # flatten
+    model.add(Flatten(input_shape=input_shape))
+    # fc layers
+    model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l2(regularization_rate)))
+    model.add(Dropout(0.4))
+    model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(regularization_rate)))
+    model.add(Dropout(0.4))
+    model.add(Dense(category_count, activation='softmax', kernel_regularizer=regularizers.l2(regularization_rate)))
+
+    return model
+
+
 # Disable Tensorflow debugging information
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 h, w, kernel_size, kernel_stride, pool_stride, pool_size_list = model_parameter.get_parameter_149_26()
-c = 2
+c = 1
 train_image_count = 100000
-input_shape = (h, w, c)
 learning_rate = 0.0001
 regularization_rate = 0.0001
 category_count = 7 + 1
-n_epoch = 300
+n_epoch = 500
 mini_batch_size = 256
 
-model = Sequential()
-
-# Layer 1
-model.add(Conv2D(32,
-                 kernel_size=kernel_size,
-                 strides=kernel_stride,
-                 activation='relu',
-                 input_shape=input_shape))
-# model.add(BatchNormalization())
-model.add(Dropout(0.3))
-model.add(MaxPooling2D(pool_size=pool_size_list[0], strides=pool_stride))
-
-# Layer 2
-model.add(Conv2D(64, kernel_size, activation='relu'))
-model.add(Dropout(0.3))
-model.add(MaxPooling2D(pool_size=pool_size_list[1]))
-# Layer 3
-model.add(Conv2D(128, kernel_size, activation='relu'))
-model.add(Dropout(0.3))
-model.add(MaxPooling2D(pool_size=pool_size_list[2]))
-
-# Layer 4
-model.add(Conv2D(256, kernel_size, activation='relu'))
-model.add(Activation('relu'))
-model.add(Dropout(0.3))
-model.add(MaxPooling2D(pool_size=pool_size_list[3]))
-
-# flatten
-model.add(Flatten(input_shape=input_shape))
-
-# fc layers
-model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l2(regularization_rate)))
-model.add(Dropout(0.4))
-model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(regularization_rate)))
-model.add(Dropout(0.4))
-model.add(Dense(category_count, activation='softmax', kernel_regularizer=regularizers.l2(regularization_rate)))
+cnn_model = get_cnn_model()
 
 # read image
 root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
@@ -71,6 +72,18 @@ mat_file_name = 'mfcc_logf_slice_150_025.mat'
 mat_path = root_path + mat_file_name
 digits = io.loadmat(mat_path)
 split_method = 'rep'
+
+# override filepath via args
+if len(sys.argv) >= 4:
+    mat_file_name = sys.argv[1]
+    split_method = sys.argv[2]
+    c = int(sys.argv[3])
+
+mat_path = root_path + mat_file_name
+digits = io.loadmat(mat_path)
+print("parameter, file path: ", mat_path)
+input_shape = (h, w, c)
+print("input_shape: ", input_shape)
 
 # redirect output to both console and txt
 sys.stdout = model_parameter.Logger(
@@ -116,27 +129,27 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
 # train
-model.compile(loss=keras.losses.categorical_crossentropy,
-              # optimizer=keras.optimizers.SGD(lr=0.01),
-              optimizer=optimizers.RMSprop(lr=learning_rate, decay=1e-5),
-              metrics=['accuracy'])
+cnn_model.compile(loss=keras.losses.categorical_crossentropy,
+                  # optimizer=keras.optimizers.SGD(lr=0.01),
+                  optimizer=optimizers.RMSprop(lr=learning_rate, decay=1e-5),
+                  metrics=['accuracy'])
 model_save_path = root_path + '/model_' + mat_file_name.split('.')[0] + '_' + split_method + '.h5'
 checkpoint = ModelCheckpoint(model_save_path, monitor='val_acc', verbose=1,
                              save_best_only=True, save_weights_only=False, mode='auto', period=1)
 
-earlystop = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=0, mode='auto',
+earlystop = EarlyStopping(monitor='val_acc', min_delta=0, patience=30, verbose=0, mode='auto',
                           restore_best_weights=True)
 callback_list = [checkpoint, earlystop]
-model.summary()
-history = model.fit(x_train, y_train,
-                    batch_size=mini_batch_size,
-                    epochs=n_epoch,
-                    verbose=2,
-                    validation_data=(x_val, y_val),
-                    callbacks=callback_list)
+cnn_model.summary()
+history = cnn_model.fit(x_train, y_train,
+                        batch_size=mini_batch_size,
+                        epochs=n_epoch,
+                        verbose=2,
+                        validation_data=(x_val, y_val),
+                        callbacks=callback_list)
 # model.save_weights(root_path + '/weight_' + mat_file_name.split('.')[0] + '_' + split_method + '.h5')
 # model.save(model_save_path)
-score = model.evaluate(x_test, y_test, verbose=1)
+score = cnn_model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
@@ -161,12 +174,12 @@ emotion_list = ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust'
 for i in range(len(normal_test_sets)):
     test_set = np.array(normal_test_sets[i])
     y_test = keras.utils.to_categorical(np.array([i] * len(test_set)), category_count)
-    score = model.evaluate(test_set, y_test, verbose=0)
+    score = cnn_model.evaluate(test_set, y_test, verbose=0)
     print('{} Test accuracy:\t{}'.format(emotion_list[i], score[1]))
 
 for i in range(len(strong_test_sets) - 1):
     i += 1
     test_set = np.array(strong_test_sets[i])
     y_test = keras.utils.to_categorical(np.array([i] * len(test_set)), category_count)
-    score = model.evaluate(test_set, y_test, verbose=0)
+    score = cnn_model.evaluate(test_set, y_test, verbose=0)
     print('{} Test accuracy:\t{}'.format(emotion_list[i], score[1]))
