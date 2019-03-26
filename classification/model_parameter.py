62c86fa233f55acab7898e4daa3a00c6fc04d5bd
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-25 23:55:06 -0400

    better parameter for 26s

--- classification/model_parameter.py
+++ classification/model_parameter.py
@@ -48,50 +48,50 @@ def select_parameter(_h, _w):
 def get_parameter_526_26():
     h = 526
     w = 26
-    kernel_size = (5, 5)
+    kernel_size = (5, 2)
     kernel_stride = (1, 1)
     pool_stride = (4, 1)
-    pool_size_list = [(4, 1), (4, 1), (2, 1), (2, 2)]
+    pool_size_list = [(4, 1), (4, 1), (4, 1), (2, 1)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
 
 
 def get_parameter_199_26():
     h = 199
     w = 26
-    kernel_size = (5, 5)
+    kernel_size = (5, 2)
     kernel_stride = (1, 1)
     pool_stride = (2, 1)
-    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    pool_size_list = [(4, 1), (4, 1), (2, 1), (2, 1)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
 
 
 def get_parameter_149_26():
     h = 149
     w = 26
-    kernel_size = (5, 5)
+    kernel_size = (5, 2)
     kernel_stride = (1, 1)
-    pool_stride = (2, 1)
-    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    pool_stride = (4, 1)
+    pool_size_list = [(4, 1), (2, 1), (2, 1), (2, 1)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
 
 
 def get_parameter_99_26():
     h = 99
     w = 26
-    kernel_size = (5, 5)
+    kernel_size = (5, 2)
     kernel_stride = (1, 1)
-    pool_stride = (2, 1)
-    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    pool_stride = (4, 1)
+    pool_size_list = [(2, 1), (2, 1), (1, 1), (2, 1)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
 
 
 def get_parameter_49_26():
     h = 49
     w = 26
-    kernel_size = (3, 3)
+    kernel_size = (3, 2)
     kernel_stride = (1, 1)
-    pool_stride = (2, 1)
-    pool_size_list = [(2, 1), (2, 1), (2, 1), (2, 2)]
+    pool_stride = (4, 1)
+    pool_size_list = [(2, 1), (1, 1), (2, 1), (2, 1)]
     return h, w, kernel_size, kernel_stride, pool_stride, pool_size_list
 
 
