701a6458de56b163b5f3963061b9fdb5ba9a2f04
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-25 23:53:42 -0400

    fix bug in svd vis

--- visualization/svd_visualization_test.py
+++ visualization/svd_visualization_test.py
@@ -3,18 +3,24 @@ from scipy import io
 import numpy as np
 from sklearn import preprocessing
 
-mat_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24\raw.mat'
+root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+file_name = 'mfcc.mat'
+mat_path = root_path + file_name
 x_axis_index = 0
 y_axis_index = 1
 digits = io.loadmat(mat_path)
-X, y = digits.get('feature_matrix'), digits.get('emotion_label')[0]  # X: nxm: n=1440//sample, m=feature
-X = X[:, ::100]
+# X, y = digits.get('feature_matrix'), digits.get('emotion_label')[0]  # X: nxm: n=1440//sample, m=feature
+# X = X[:, ::100]
+X, y = digits.get('feature_matrix'), digits.get('statement_label')[0]
+t = 1
+X = X[:, :, t:t + 1]
+X = X.reshape(X.shape[0], -1)
 n_samples, n_features = X.shape
 print("{} samples, {} features".format(n_samples, n_features))
 
 eigenvalues, eigenvectors = np.linalg.eig(np.cov(X))  # values: nx1/1440x1, vectors: nxn/1440x1440
 
-U, s, Vh = np.linalg.svd(X.transpose(), full_matrices=True)  # u: mxm, s: mx1, v:nxn/1440x1440
+U, s, Vh = np.linalg.svd(X.transpose(), full_matrices=False)  # u: mxm, s: mx1, v:nxn/1440x1440
 # s[2:] = 0
 
 fig = plt.figure()
@@ -40,8 +46,8 @@ ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 plt.xlabel('Projection on {}'.format(x_axis_index))
 plt.ylabel('Projection on {}'.format(y_axis_index))
-ev1 = Vh.transpose()[x_axis_index].real  # ev: nx1/1440x1
-ev2 = Vh.transpose()[y_axis_index].real
+ev1 = np.absolute(Vh[x_axis_index])  # ev: nx1/1440x1
+ev2 = np.absolute(Vh[y_axis_index])
 xx = X.transpose().dot(ev1)  # mxn.nx1 = mx1
 yy = X.transpose().dot(ev2)
 small_edge = (max(yy) - min(yy)) * small_edge_index
@@ -64,7 +70,7 @@ ax.axvline(x=0, color='k')
 plt.xlabel('Correlation on {}'.format(x_axis_index))
 plt.ylabel('Correlation on {}'.format(y_axis_index))
 s_x = preprocessing.normalize(X.transpose())
-normalized_vh = preprocessing.normalize(Vh.real.transpose())
+normalized_vh = preprocessing.normalize(np.absolute(Vh))
 s_ev1 = normalized_vh[x_axis_index]
 s_ev2 = normalized_vh[y_axis_index]
 xx = s_x.dot(s_ev1)
@@ -83,7 +89,7 @@ for i in range(xx.shape[0]):
 ax.set_title('features_correlation')
 
 eigenvalues, eigenvectors = np.linalg.eig(np.cov(X.transpose()))  # values: mx1/12x1, vectors: mxm/12x12
-U, s, Vh = np.linalg.svd(X, full_matrices=True)  # u: nxn/1440x1440, s: mx1, v:mxm
+U, s, Vh = np.linalg.svd(X, full_matrices=False)  # u: nxn/1440x1440, s: mx1, v:mxm
 # s[2:] = 0
 
 ax = fig.add_subplot(325)
@@ -92,8 +98,8 @@ ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 plt.xlabel('Projection on {}'.format(x_axis_index))
 plt.ylabel('Projection on {}'.format(y_axis_index))
-ev1 = Vh.transpose()[x_axis_index].real  # ev: nx1/1440x1
-ev2 = Vh.transpose()[y_axis_index].real
+ev1 = np.absolute(Vh[x_axis_index])  # ev: nx1/1440x1
+ev2 = np.absolute(Vh[y_axis_index])
 xx = X.dot(ev1)  # nxm.mx1=nx1
 yy = X.dot(ev2)
 small_edge = (max(yy) - min(yy)) * small_edge_index
@@ -103,7 +109,7 @@ small_edge = (max(xx) - min(xx)) * small_edge_index
 ax.set_xlim(min(xx) - small_edge if min(xx) <= -small_edge else -small_edge,
             max(xx) + small_edge if max(xx) >= small_edge else small_edge)
 for i in range(X.shape[0]):
-    ax.text(xx[i], yy[i], str(y[i]), color=plt.cm.tab20(int(y[i])), fontdict={'size': 8})
+    ax.text(xx[i], yy[i], str(y[i] % 2 + 1), color=plt.cm.tab20(int(y[i] % 2 + 1)), fontdict={'size': 8})
 ax.set_title('samples_projection')
 
 ax = fig.add_subplot(326)
@@ -113,7 +119,7 @@ ax.axvline(x=0, color='k')
 plt.xlabel('Correlation on {}'.format(x_axis_index))
 plt.ylabel('Correlation on {}'.format(y_axis_index))
 s_x = preprocessing.normalize(X)
-normalized_vh = preprocessing.normalize(Vh.real.transpose())
+normalized_vh = preprocessing.normalize(np.absolute(Vh))
 s_ev1 = normalized_vh[x_axis_index]
 s_ev2 = normalized_vh[y_axis_index]
 xx = s_x.dot(s_ev1)
@@ -125,7 +131,7 @@ small_edge = (max(xx) - min(xx)) * small_edge_index
 ax.set_xlim(min(xx) - small_edge if min(xx) <= -small_edge else -small_edge,
             max(xx) + small_edge if max(xx) >= small_edge else small_edge)
 for i in range(X.shape[0]):
-    ax.text(xx[i], yy[i], str(y[i]), color=plt.cm.tab20(int(y[i])), fontdict={'size': 8})
+    ax.text(xx[i], yy[i], str(y[i] % 2 + 1), color=plt.cm.tab20(int(y[i] % 2 + 1)), fontdict={'size': 8})
 ax.set_title('samples_correlation')
 
 plt.show()
