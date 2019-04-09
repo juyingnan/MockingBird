b138dfab81fba2ae882a6acb2e0b1510bb1073ca
Yingnan Ju <bunny_gg@hotmail.com>
2019-04-09 15:16:19 -0400

    added grid search for svm

--- classification/svm_test.py
+++ classification/svm_test.py
@@ -3,6 +3,7 @@ from sklearn.metrics import confusion_matrix
 from scipy import io
 from sklearn.model_selection import train_test_split
 import dataset_split
+from sklearn.model_selection import GridSearchCV
 
 # X = [[0], [1], [2], [3]]
 # Y = [0, 1, 2, 3]
@@ -15,7 +16,7 @@ train_data, train_label, test_data, test_label, normal_test_sets, strong_test_se
     dataset_split.train_test_rep_split4(digits, 1, split_method)
 
 # try filter some mfcc
-wanted_columns = [x for x in range(26) if x not in ([] + [6,13,16,19,20])]
+wanted_columns = [x for x in range(26) if x not in ([] + [13, 16])]
 train_data = train_data[:, :, wanted_columns]
 test_data = test_data[:, :, wanted_columns]
 
@@ -23,8 +24,16 @@ train_data = train_data.reshape(train_data.shape[0], -1)
 test_data = test_data.reshape(test_data.shape[0], -1)
 X_train, X_test, Y_train, Y_test = train_data, test_data, train_label, test_label
 
-clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+svc = svm.SVC()
+clf = GridSearchCV(svc, tuned_parameters)
 clf.fit(X_train, Y_train)
+print(clf.best_params_)
+
+# clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+# clf.fit(X_train, Y_train)
 
 svm_prediction = clf.predict(X_test)
 accuracy = clf.score(X_test, Y_test)
