96830d10d9c11f5a042e150e5344dc46411f9b37
Yingnan Ju <bunny_gg@hotmail.com>
2019-04-11 12:01:33 -0400

    added possibility to discard both layer/eigen

--- classification/svm_test.py
+++ classification/svm_test.py
@@ -1,9 +1,10 @@
+from scipy import io
 from sklearn import svm
 from sklearn.metrics import confusion_matrix
-from scipy import io
-from sklearn.model_selection import train_test_split
-import dataset_split
 from sklearn.model_selection import GridSearchCV
+
+import dataset_split
+import mat_reconstruct_svd
 
 # X = [[0], [1], [2], [3]]
 # Y = [0, 1, 2, 3]
@@ -16,21 +17,24 @@ train_data, train_label, test_data, test_label, normal_test_sets, strong_test_se
     dataset_split.train_test_rep_split4(digits, 1, split_method)
 
 # try filter some mfcc
-wanted_columns = [x for x in range(26) if x not in ([] + [13, 16])]
-train_data = train_data[:, :, wanted_columns]
-test_data = test_data[:, :, wanted_columns]
+# wanted_columns = [x for x in range(26) if x not in ([] + [13, 16])]
+# train_data = train_data[:, :, wanted_columns]
+# test_data = test_data[:, :, wanted_columns]
 
 train_data = train_data.reshape(train_data.shape[0], -1)
 test_data = test_data.reshape(test_data.shape[0], -1)
+# train_data = mat_reconstruct_svd.eigen_discard(train_data, [1], need_reshape=False)
+# test_data = mat_reconstruct_svd.eigen_discard(test_data, [1], need_reshape=False)
 X_train, X_test, Y_train, Y_test = train_data, test_data, train_label, test_label
 
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-                     'C': [1, 10, 100, 1000]},
-                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
-svc = svm.SVC()
-clf = GridSearchCV(svc, tuned_parameters)
+# tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+#                      'C': [1, 10, 100, 1000]},
+#                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+tuned_parameters = [{'kernel': ['linear'], 'C': [1, ]}]
+
+clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
 clf.fit(X_train, Y_train)
-print(clf.best_params_)
+#print(clf.best_params_)
 
 # clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
 # clf.fit(X_train, Y_train)
