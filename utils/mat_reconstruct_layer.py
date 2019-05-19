0bcaee22dd84b845a44b9ff704b39d98c23dd097
Yingnan Ju <bunny_gg@hotmail.com>
2019-05-19 03:14:51 -0400

    finished mat reconstruction

---
+++ utils/mat_reconstruct_layer.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sys
+from scipy import io
+
+
+def layer_discard(matrix, discard_list):
+    result = np.copy(matrix)
+    for index in discard_list:
+        result[:, :, index] = 0
+    return result
+
+
+def layer_discard_overwrite(matrix, discard_list):
+    result = np.copy(matrix)
+    result = np.delete(result, discard_list, 2)
+    zero_fill = np.zeros((result.shape[0], result.shape[1], len(discard_list)))
+    result = np.append(result, zero_fill, axis=2)
+    return result
+
+
+if __name__ == '__main__':
+    root_path = r'D:\Projects\emotion_in_speech\vis_mat/'
+    file_name = 'mfcc.mat'
+    to_discard_list = [1, 3, 5, 7, 9]
+
+    if len(sys.argv) >= 3:
+        file_name = sys.argv[1] + '.mat'
+        to_discard_list = [int(item) for item in sys.argv[2:]]
+        print(to_discard_list)
+
+    mat_path = root_path + file_name
+    result_file_name_postfix = '-'.join([str(item) for item in to_discard_list])
+    digits = io.loadmat(mat_path)
+    X = digits.get('feature_matrix')
+    print(X.shape)
+
+    reconstruct_mat = layer_discard(X, to_discard_list)
+    reconstruct_mat_overwrite = layer_discard_overwrite(X, to_discard_list)
+
+    digits['feature_matrix'] = np.array(reconstruct_mat)
+    io.savemat(mat_path.replace('.mat', '_layer_%s.mat' % result_file_name_postfix), mdict=digits)
+
+    digits['feature_matrix'] = np.array(reconstruct_mat_overwrite)
+    io.savemat(mat_path.replace('.mat', '_layer_%s_ow.mat' % result_file_name_postfix), mdict=digits)
--- utils/mat_reconstruct_svd.py
+++ utils/mat_reconstruct_svd.py
@@ -1,5 +1,5 @@
 import numpy as np
-from bokeh.io import output_file
+import sys
 from scipy import io
 
 
@@ -15,25 +15,26 @@ def eigen_discard(matrix, discard_list, need_reshape=False):
 
 
 if __name__ == '__main__':
-    root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+    root_path = r'D:\Projects\emotion_in_speech\vis_mat/'
     file_name = 'mfcc.mat'
-    output_file("result/svd.html")
+    to_discard_list = [1]
+
+    if len(sys.argv) >= 3:
+        file_name = sys.argv[1] + '.mat'
+        to_discard_list = [int(item) for item in sys.argv[2:]]
+        print(to_discard_list)
+
     mat_path = root_path + file_name
-    x_axis_index = 0
-    y_axis_index = 1
+    result_file_name_postfix = '-'.join([str(item) for item in to_discard_list])
     digits = io.loadmat(mat_path)
-    # X, y = digits.get('feature_matrix'), digits.get('emotion_label')[0]  # X: nxm: n=1440//sample, m=feature
-    # X = X[:, ::100]
     X = digits.get('feature_matrix')
-    # t = 13
-    # X = X[:, :, t:t + 1]
-    X = X.reshape(X.shape[0], -1)
-    n_samples, n_features = X.shape
-    print("{} samples, {} features".format(n_samples, n_features))
+    original_shape = X.shape
+    print("Original shape: {}".format(original_shape))
 
-    # eigenvalues, eigenvectors = np.linalg.eig(np.cov(X.transpose()))  # values: mx1/12x1, vectors: mxm/12x12
-    U, s, Vh = np.linalg.svd(X, full_matrices=False)  # u: nxn/1440x1440, s: mx1, v:mxm
-    s[1] = 0
+    reconstruct_mat = eigen_discard(X, to_discard_list, need_reshape=True)
+    print("Reconstructed shape: {}".format(reconstruct_mat.shape))
+    reconstruct_mat = reconstruct_mat.reshape(original_shape)
 
-    reconstruct_mat = np.dot(U * s, Vh)
-    print(reconstruct_mat.shape)
+    # print(np.allclose(X, reconstruct_mat))
+    digits['feature_matrix'] = np.array(reconstruct_mat)
+    io.savemat(mat_path.replace('.mat', '_eigen_%s.mat' % result_file_name_postfix), mdict=digits)
