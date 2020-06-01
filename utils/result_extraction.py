3a73e04e510fb006f6c6524f5c5e4a251edcb428
Yingnan Ju <bunny_gg@hotmail.com>
2020-05-31 22:38:44 -0400

    added a script to extract conten from result

--- utils/result_extraction.py
+++ utils/result_extraction.py
@@ -11,20 +11,28 @@ def extract_accuracy(path):
     return 'N/A'
 
 
-root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+# root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
+#
+# for phase in ['predict', 'train']:
+#     for split_method in ['rep', 'sen']:
+#         for method in ['stft', 'mfcc', 'logf', 'mfcc_logf']:
+#             print(phase, split_method, method)
+#             for length in ['050', '100', '150']:
+#                 result = []
+#                 for step in ['025', '050', '075']:
+#                     file_name = 'log_' + phase + '_' + method + '_slice_' + \
+#                                 length + '_' + step + '_' + split_method + '.log'
+#                     file_path = root_path + file_name
+#                     result.append(extract_accuracy(file_path))
+#                 print('\t'.join(result))
+#             raw_file_path = root_path + 'log_' + phase + '_' + method + '_' + split_method + '.log'
+#             print(extract_accuracy(raw_file_path))
+#             print()
 
-for phase in ['predict', 'train']:
-    for split_method in ['rep', 'sen']:
-        for method in ['stft', 'mfcc', 'logf', 'mfcc_logf']:
-            print(phase, split_method, method)
-            for length in ['050', '100', '150']:
-                result = []
-                for step in ['025', '050', '075']:
-                    file_name = 'log_' + phase + '_' + method + '_slice_' + \
-                                length + '_' + step + '_' + split_method + '.log'
-                    file_path = root_path + file_name
-                    result.append(extract_accuracy(file_path))
-                print('\t'.join(result))
-            raw_file_path = root_path + 'log_' + phase + '_' + method + '_' + split_method + '.log'
-            print(extract_accuracy(raw_file_path))
-            print()
+root_path = r'C:\Users\bunny\Desktop\data/'
+
+file_names = os.listdir(root_path)
+for file_name in file_names:
+    file_path = root_path + file_name
+    result = extract_accuracy(file_path)
+    print(file_name, '\t', result)
