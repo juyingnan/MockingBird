acfb51eeb94fd1218d8ac040cdd22a3dd7205f2d
Yue Chen <yc59@indiana.edu>
2021-03-09 22:39:17 -0500

    added feature_matrix_vis.py

---
+++ visualization/feature_matrix_vis.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+
+def explode(data):
+    size = np.array(data.shape)*2
+    data_e = np.zeros(size - 1, dtype=data.dtype)
+    data_e[::2, ::2, ::2] = data
+    return data_e
+
+# build up the numpy logo
+n_voxels = np.zeros((7, 8, 9), dtype=bool)
+n_voxels[:, 0, :] = True
+# n_voxels[0, 0, :] = True
+# n_voxels[-1, 0, :] = True
+# n_voxels[1, 0, 2] = True
+# n_voxels[2, 0, 1] = True
+facecolors = np.where(n_voxels, 'green', 'white')
+edgecolors = np.where(n_voxels, 'black', 'black')
+filled = np.ones(n_voxels.shape)
+
+# upscale the above voxel image, leaving gaps
+filled_2 = explode(filled)
+fcolors_2 = explode(facecolors)
+ecolors_2 = explode(edgecolors)
+
+# Shrink the gaps
+x, y, z = np.indices(np.array(filled_2.shape) + 1).astype(float) // 2
+x[0::2, :, :] += 0.05
+y[:, 0::2, :] += 0.05
+z[:, :, 0::2] += 0.05
+x[1::2, :, :] += 0.95
+y[:, 1::2, :] += 0.95
+z[:, :, 1::2] += 0.95
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.voxels(x, y, z, filled_2, facecolors=fcolors_2, edgecolors=ecolors_2)
+ax.set_xlim3d(0, 10)
+ax.set_ylim3d(0, 10)
+ax.set_zlim3d(0, 10)
+plt.show()
