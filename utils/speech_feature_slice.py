a1413c5926d9d41bd2b4037b28a36bbde985a6d3
juyingnan <bunny_gg@hotmail.com>
2024-03-13 16:40:53 -0700

    updated printing

--- utils/speech_feature_slice.py
+++ utils/speech_feature_slice.py
@@ -3,6 +3,7 @@ from python_speech_features import mfcc
 from python_speech_features import logfbank
 from scipy import io, signal
 from tqdm import tqdm
+import os
 
 
 def get_slice_feature(original_mat_path):
@@ -14,7 +15,7 @@ def get_slice_feature(original_mat_path):
     stft_list = []
     mfcc_list = []
     logfbank_list = []
-    all_list = []
+    mfcc_fbank_list = []
     count = 0
     for i in tqdm(range(len(x)), desc="Processing"):
         _data = x[i]
@@ -30,35 +31,50 @@ def get_slice_feature(original_mat_path):
         _logfbank = logfbank(_data, _sr, nfft=2048, nfilt=26)
 
         # mfcc + logfbank
-        _all = np.dstack((_mfcc, _logfbank))
+        _mfcc_fbank = np.dstack((_mfcc, _logfbank))
 
         stft_list.append(np.absolute(spectrogram.T))
         mfcc_list.append(_mfcc)
         logfbank_list.append(_logfbank)
-        all_list.append(_all)
+        mfcc_fbank_list.append(_mfcc_fbank)
 
         count += 1
         print("\rreading {0}/{1}".format(count, len(x)), end='')
 
     print('stft shape: ', np.array(stft_list[0]).shape)
-    print('mfcc+logfbank shape: ', np.array(all_list[0]).shape)
+    print('mfcc+logfbank shape: ', np.array(mfcc_fbank_list[0]).shape)
 
     # 49 x 26 x 2
     digits['feature_matrix'] = np.array(stft_list)
-    io.savemat(original_mat_path.replace('raw', 'stft'), mdict=digits)
+    stft_mat_path = original_mat_path.replace('raw', 'stft')
+    print('saving to:', stft_mat_path)
+    io.savemat(stft_mat_path, mdict=digits)
+
     digits['feature_matrix'] = np.array(mfcc_list)
-    io.savemat(original_mat_path.replace('raw', 'mfcc'), mdict=digits)
+    mfcc_mat_path = original_mat_path.replace('raw', 'mfcc')
+    print('saving to:', mfcc_mat_path)
+    io.savemat(mfcc_mat_path, mdict=digits)
+
     digits['feature_matrix'] = np.array(logfbank_list)
-    io.savemat(original_mat_path.replace('raw', 'logf'), mdict=digits)
-    digits['feature_matrix'] = np.array(all_list)
-    io.savemat(original_mat_path.replace('raw', 'mfcc_logf'), mdict=digits)
+    logf_mat_path = original_mat_path.replace('raw', 'logf')
+    print('saving to:', logf_mat_path)
+    io.savemat(logf_mat_path, mdict=digits)
+
+    digits['feature_matrix'] = np.array(mfcc_fbank_list)
+    mfcc_fbank_mat_path = original_mat_path.replace('raw', 'mfcc_logf')
+    print('saving to:', mfcc_fbank_mat_path)
+    io.savemat(mfcc_fbank_mat_path, mdict=digits)
 
 
-root_path = r'D:\Projects\emotion_in_speech\Audio_Speech_Actors_01-24/'
-get_slice_feature(root_path + 'raw.mat')
-for length in ['050', '100', '150']:
-    for step in ['025', '050', '075']:
-        slice_parameter = length + '_' + step
-        mat_file_name = 'raw_slice_' + slice_parameter + '.mat'
-        mat_path = root_path + mat_file_name
-        get_slice_feature(mat_path)
+for is_norm in [True, False]:
+    root_path = r'D:\Projects\emotion_in_speech\CREMA-D\mat/'
+    mat_name = 'norm_raw.mat' if is_norm else 'raw.mat'
+    mat_path = os.path.join(root_path, mat_name)
+    get_slice_feature(mat_path)
+    for length in ['050', '100', '150']:
+        for step in ['025', '050', '075']:
+            slice_parameter = length + '_' + step
+            mat_file_name = 'raw_slice_' + slice_parameter + '.mat'
+            mat_file_name = 'norm_' + mat_file_name if is_norm else mat_file_name
+            mat_path = os.path.join(root_path, mat_file_name)
+            get_slice_feature(mat_path)
